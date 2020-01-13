cc9fd026037c5cb7a4eede6ca196e3512bfa1773
Lachlan <lachlandufort-kennett@outlook.com>
2020-01-13 17:44:56 +0000

    added count method

--- code-counter.py
+++ code-counter.py
@@ -8,8 +8,10 @@ class CounterNode:
 		self.parent_node = parent
 		self.child_nodes = []
 		self.files = []
+		self.line_counts = []
 
 		self.index()
+		self.count()
 		print(self)
 
 	def __repr__(self):
@@ -17,7 +19,8 @@ class CounterNode:
 
 	def index(self):
 		_, dirnames, filenames = next(os.walk(self.path))
-		self.files = filenames
+		for filename in filenames:
+			self.files.append(os.path.join(self.path, filename))
 
 		for directory in ignored_directories:
 			if directory in dirnames:
@@ -28,9 +31,28 @@ class CounterNode:
 			self.child_nodes.append(new_child)
 
 	def count(self):
-		pass
+		for file in self.files:
+			line_count = 0
+			with open(file) as f:
+				try:
+					for _ in f:
+						line_count += 1
+				except UnicodeDecodeError:
+					pass  # TODO: Fix this
+			if self.parent_node is not None:  # TODO: get rid of this, identifier for master node
+				notfound = True
+				master_node = self.parent_node
+				while notfound:
+					if master_node.parent_node is None:
+						notfound = False
+					else:
+						master_node = master_node.parent_node
+				master_node.line_counts.append((file, line_count))
+			else:
+				self.line_counts.append((file, line_count))
 
 
 if __name__ == "__main__":
 	ignored_directories = [".git", ".idea", "__pycache__", "dist", "build"]
 	master = CounterNode(input("Root directory: "))
+	print(master.line_counts)
